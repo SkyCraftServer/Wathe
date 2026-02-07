a35b11b887816f465664631a364c5f5fa9775bec
CuteLittleSky <62592731+CuteLittleSky@users.noreply.github.com>
2026-02-07 16:36:56 +0800

    Update MapVariablesWorldComponent.java

--- src/main/java/dev/doctor4t/wathe/cca/MapVariablesWorldComponent.java
+++ src/main/java/dev/doctor4t/wathe/cca/MapVariablesWorldComponent.java
@@ -95,7 +95,7 @@ public class MapVariablesWorldComponent implements AutoSyncedComponent {
         positions.add(new PosWithOrientation(-6.5, 123, -535.5, 178, 0));
         positions.add(new PosWithOrientation(-11.5, 122, -536.5, -45, 40));
         positions.add(new PosWithOrientation(8.5, 122, -537.5, 179, -25));
-        positions.add(new PosWithOrientation(17.5, 123, -539.5, 0, 0));
+        positions.add(new PosWithOrientation(17.5, 123, -538.5, 0, 0));
         positions.add(new PosWithOrientation(12.5, 123, -537.5, 0, -15));
         positions.add(new PosWithOrientation(26.5, 122, -537.5, 179, -10));
         positions.add(new PosWithOrientation(36.5, 123, -536.5, 0, 0));
